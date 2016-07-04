e87278d06b6062f6ad343925da00b4c776e3ba7b
yvkschaefer <yvkschaefer@gmail.com>
2016-07-04 20:45:49 +0000

    Added Functions format

--- coding-style.js
+++ coding-style.js
@@ -15,3 +15,24 @@ if (num > 0) { // <- always have {} even if you have one statement. if/else/for/
 if (num > 0) {
     // .. code goes here
 }
+
+// Functions
+
+/*
+These three different ways of declaring functions are not 100% equivalent.
+We will see together the differences between them.
+
+Use the first version.
+*/
+
+function doSomething(sentence) {
+    // ... code goes here. USE THIS FORMAT :)
+}
+
+var doSomething = function(sentence) {
+    // ... code goes here
+}
+
+var doSomething = function doSomething(sentence) {
+    // ... code goes here
+}
